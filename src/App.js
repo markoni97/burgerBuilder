27f39a279bf3af1a3dd1386e7cb423d1061d5567
Marko Radenkovic <code.markoni97@gmail.com>
2021-03-18 16:30:56 +0100

    Added Burger, BurgerIngredients, BurgerBuilder and some other components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import classes from './App.module.css';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
 
-  render(){
+  render() {
     return (
       <div className={classes.App}>
-        <h1>Welcome to my React App</h1>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
